e064a465a72d580098cfc70184ac89cc18e10e9f
ALBERT\user <a0976720616@gmail.com>
2020-10-27 20:40:18 +0800

    用顏色區分單雙行資料

--- inquire.php
+++ inquire.php
@@ -6,19 +6,26 @@
 </head>
 <body bgcolor="#48D1CC"　text="#0000EE"  >
  <title >叫修紀錄查詢</title>
- <a href="http://127.0.0.1/myphp.html" >
-  <img src="129.png" width="80px" height="40px">
+ <a href="http://127.0.0.1/myproject/myphp.html" >
+  <img src="129.png" width="100px" height="60px">
  </a>
  <hr size="5" align="left" noshade width="90%" color="#1A1A1A">
  
  
  <form method="POST" action=" <?php echo $_SERVER['PHP_SELF'];?>">
    <div class="search_center">  
-     <input type="search" name="search" placeholder="輸入關鍵字"  size="60">
+     <input type="search" name="search" placeholder="輸入關鍵字"  size="60" required>
      <input type="submit"  value="搜尋" ></br>
    </div> 
 </form>
-   
+<style type="text/css">
+     .search_center
+     {
+         text-align: center;
+     }
+    .odd {background:	#53FF53;}
+    .even {background:#A6FFA6;}
+   </style>
  
  <?php
  $host = 'localhost';
@@ -31,7 +38,7 @@
  
  $connect = new mysqli($host, $user, $passwd, $database);
  
- echo "<table align='center' border=2px  bordercolor='#1A1A1A' background='#FFFFFF'>";
+ echo "<table align='center' border=2px  bordercolor='#1A1A1A' ";
 
  if (!$connect) 
  {
@@ -47,7 +54,7 @@
  if($_SERVER["REQUEST_METHOD"] == "POST")
  {
   $sqldate="SELECT date1 FROM fix   " ;
-  $results=mysqli_query($connect,$sqldate);
+  //$results=mysqli_query($connect,$sqldate);
   $shopname=$_POST['search'];
   $selectSql = "SELECT * FROM fix where shopname like '%$shopname%' or date1 like '%$shopname%' or engineer like '%$shopname%' " ;
   $result=mysqli_query($connect,$selectSql);
@@ -60,18 +67,29 @@
  //$fixData = $connect->query($selectSql);
  //如果返回的是多條資料，函式 fetch_assoc() 將結合集放入到關聯陣列並迴圈輸出。 while() 迴圈出結果集，並輸出 Id，Rank，Name，ATK和HP 四個欄位值。
 
-
+ $i=1;
  
- echo "<tr align='center' background='#000000'><th>日期</th><th>店櫃</th><th>問題</th><th>解法</th><th>備註</th><th>工程師</th></tr>";
+ echo "<tr align='center' background='#000000'><th width='150'>日期</th><th width='150'>店櫃</th><th width='200'>問題</th><th width='200'>解法</th><th width='150'>備註</th><th width='150'>工程師</th></tr>";
  if (mysqli_num_rows($result)>0) 
  { 
      while ($row = mysqli_fetch_assoc($result))
-     { echo "<tr background='#000000'>";
+     { 
+       if($i%2==1)
+       {
+       echo "<tr class='odd'>";
        echo "<td width='150' align='center'>". $row["date1"] . "</td><td width='150' align='center'>". $row["shopname"] . "</td><td width='200'>" . $row["problem"] . "</td><td width='200'>" . $row["solution"] . "</td><td width='200'>" . $row["remark"] . "</td><td width='150' align='center'>".$row["engineer"]."</td>";       
        echo "</tr>"; 
-      
-      
+       }
+       else
+       {
+        echo "<tr class='even'>";
+        echo "<td width='150' align='center'>". $row["date1"] . "</td><td width='150' align='center'>". $row["shopname"] . "</td><td width='200'>" . $row["problem"] . "</td><td width='200'>" . $row["solution"] . "</td><td width='200'>" . $row["remark"] . "</td><td width='150' align='center'>".$row["engineer"]."</td>";       
+        echo "</tr>";
+       }
+      $i++;
      }
+
+
      
  } 
  else
@@ -80,13 +98,7 @@
  }
 }
  ?>
- <style type="text/css">
-     .search_center{
-         text-align: center;
-       table {background:pink;}
-     }
 
-   </style>
 
 </body>
 </html>
--- insert.php
+++ insert.php
@@ -1,3 +1,4 @@
+
 <?php
 $host = 'localhost';
 //改成你登入phpmyadmin帳號
@@ -12,7 +13,7 @@ $connect = new mysqli($host, $user, $passwd, $database);
 if ($connect->connect_error) {
     die("連線失敗: " . $connect->connect_error);
 }
-echo "連線成功" ;
+//echo "連線成功" ;
 echo '<br>';
 //設定連線編碼，防止中文字亂碼
 $connect->query("SET NAMES 'utf8'");
@@ -20,29 +21,31 @@ $connect->query("SET NAMES 'utf8'");
 
 
 $datetime=date("Y-m-d H:i:s",time()+6*60*60);
-echo 'test';
-$shopname=$_GET['shopname'];
-$problem=$_GET['problem'];
-$solution=$_GET['solution'];
-$engineer=$_GET['address'];
-$remark=$_GET['remark'] ?? '';
+$shopname=$_POST['shopname'];
+$problem=$_POST['problem'];
+$solution=$_POST['solution'];
+$engineer=$_POST['address'];
+$remark=$_POST['remark'] ?? '';
 /*if ($remark) {
   $remark = '';
 }
 依樣的意思*/
 
 
-$insertSql = "INSERT INTO fix (date1,shopname,problem,solution,remark,engineer) VALUES ('{$datetime}','{$shopname}','{$problem}','{$solution}','{$remark}','{$engineer}')" ;
+$insertSql = "INSERT INTO fix (shopname,problem,solution,remark,engineer) VALUES ('{$shopname}','{$problem}','{$solution}','{$remark}','{$engineer}')" ;
 //呼叫query方法(SQL語法)
 
 $status = $connect->query($insertSql);
  
 if ($status)
 {
-  echo '新增成功';
+  echo '新增成功'.'<br>';
 } 
 else 
 {
     echo "錯誤: " . $insertSql . "<br>" . $connect->error;
 }
 ?>
+<a href="http://127.0.0.1/myproject/inquire.php" >
+    <input type="button" value="回查詢首頁">
+   </a>

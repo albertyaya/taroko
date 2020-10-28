e34789ec6239ced8f55de4b2561a85c8a38852c6
ALBERT\user <a0976720616@gmail.com>
2020-10-28 22:38:53 +0800

    no message

--- login.php
+++ login.php
@@ -28,16 +28,17 @@ echo "連線成功";
 
 
 $name = $_POST['name'];//post獲得使用者名稱錶單值
-$passowrd = $_POST['password'];//post獲得使用者密碼單值
-$message = '表單填寫不完整';
-if ($name == '' || $password == '') {
+$pa = $_POST['password'];//post獲得使用者密碼單值;
+
+
+if ($name === '' || $pa === '') {
     echo "表單填寫不完整";
-    jumpWeb();
+   // jumpWeb();
 }
 
-$sql = "select * from userdata where username = '$name' and password='$passowrd'";//檢測資料庫是否有對應的username和password的sql
+$sql = "select * from userdata where username = '$name' and password='$pa'";//檢測資料庫是否有對應的username和password的sql
 $result=mysqli_query($connect, $sql);//執行sql
-
+echo $sql;
 if(mysqli_num_rows($result))
 {//0 false 1 true
     // header('Location: http://127.0.0.1/myproject/inquire.php');
@@ -45,7 +46,7 @@ if(mysqli_num_rows($result))
     echo "登入成功";
 } else {
     //如果使用者名稱或密碼有空
-    echo "表單填寫不完整";
-    jumpWeb();
+    echo "error";
+    //jumpWeb();
 }
 ?>
